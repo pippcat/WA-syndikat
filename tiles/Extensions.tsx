19e2b93f9e76a054a9950dd0270250806ea43162
pippcat <sendyourspamhere+github@posteo.de>
2021-02-17 15:20:58 +0100

    blubb

---
+++ tiles/Extensions.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Extensions" tilewidth="32" tileheight="32" tilecount="40" columns="10">
+ <image source="../../../temp/WeltiConRoom-master/Tiles/extensions.png" width="320" height="128"/>
+ <tile id="12">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
