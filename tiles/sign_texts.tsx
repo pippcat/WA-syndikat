02368a6a33669c43624320cd94225bd003d6b8ca
pippcat <sendyourspamhere+github@posteo.de>
2021-02-17 15:45:17 +0100

    blubb

--- tiles/sign_texts.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="sign_texts" tilewidth="32" tileheight="32" tilecount="80" columns="8">
- <image source="sign_texts.png" width="256" height="320"/>
-</tileset>
